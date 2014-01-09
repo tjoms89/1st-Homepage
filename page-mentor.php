38ea79ea7b8d508b49aabce9f72f9294a40943bb
itp1tkl <thomas.kleinert@it-power.org>
2014-01-09 15:44:41 +0100

    update

--- page-mentor.php
+++ page-mentor.php
@@ -1,34 +1,16 @@
+
 <?php get_header(); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-        <div id="intro-small" class="boat">
+        <div id="intro-small" style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/boat.jpg) no-repeat center; background-color: #fff;">
+        </div>
+        
+        <div id="main">        
             <div class="row">
-                <div class="background5">
-                    <h1>Mentorenprogramm</h1>
-                </div>
-            </div>
-        </div>
-        </div>
-        <div id="main">
-            <div class="domtab">
-                <ul class="domtabs">
-                    <li><a href="#t1">Test 1</a></li>
-                    <li><a href="#t2">Test 2</a></li>
-                </ul>
-
-                <div>
-                    <h2><a name="t1" id="t1">Proof 1</a></h2>
-                    <p>Test to prove that more than one menu is possible</p>
-                    <p><a href="#top">back to menu</a></p>
-
-                </div>
-
-                <div>
-                    <h2><a name="t2" id="t2">Proof 2</a></h2>
-                    <p>Test to prove that more than one menu is possible</p>
-                    <p><a href="#top">back to menu</a></p> 
-                </div>
-
+                <br>
+                <div class="col-md-12">
+                    <?php the_content(); ?> 
+                <br><br>
             </div>
         </div>
         </div>
