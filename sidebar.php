b0983785d37daf62d406bbb2b88e353cb7db9ad1
itp1tkl <thomas.kleinert@it-power.org>
2014-01-28 16:41:01 +0100

    update

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
-<!-
+
 <div class="col-md-12" style="background:#c0c6dd;"> 
     <h3>Neuigkeiten <br> Übersicht</h3>
      <ul class="ulsidebar" style="padding-left:0px; text-align: left;">
-        <?php  wp_get_archives('type=postbypost&limit=10','format => custom','before => <li><a href="', 'after ="> </a><br><br></li>'); ?>
+        <?php  wp_get_archives('type=postbypost&limit=10','format => custom','before => <li><a href="', 'after ="> </a></li>'); ?>
      </ul>
 </div> 
 
