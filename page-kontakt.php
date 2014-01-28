b083a8283ae0e3a8f4c1eeebaabc7df2cddb9924
itp1tkl <thomas.kleinert@it-power.org>
2014-01-28 11:48:33 +0100

    update lisa.jpg

--- page-kontakt.php
+++ page-kontakt.php
@@ -12,7 +12,7 @@
                                 <h3>Sven Ladewig</h3>	 	 
                                 <p style="font-weight: bold;">Geschäftsführer</p>
                                 <p style="font-weight: bold;">Leiter Software Engineering<p>
-                                <p style="font-weight: bold;">Leiter Vertrieb/Marketing <p>
+                                <p style="font-weight: bold;">Leiter Vertrieb / Marketing <p>
                                     <br>
                                 <p>Tel.: +49 (0)461 487-3424</p>
                                 <p>Fax: +49 (0)461 487-2424</p>
