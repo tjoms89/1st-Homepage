4f2f2630bb296ab25af55ceb926c9a2e2b9210f9
itp1tkl <thomas.kleinert@it-power.org>
2014-01-29 14:53:55 +0100

    update

--- page-homepage.php
+++ page-homepage.php
@@ -4,6 +4,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
         <div id="intro" class="mainpic" >
+            <br><br>
             <div class="row">
                 <div class="col-md-4"></div>
                 <div class="col-md-4">
@@ -14,46 +15,53 @@
                 <div class="col-md-4"></div>
             </div>
             <div class="row">
-                <br><br>
-                <div class="col-sm-6 col-md-3">
-
-                    <div class="user"> </div>
-                    <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/consulting')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/user.png" alt="user" width="150" height="150" class="alignnone size-thumbnail wp-image-185" /></a>
-                    <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/consulting')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/user3.png" alt="user2" width="150" height="150" class="alignnone size-thumbnail wp-image-185" /></a>
-                    <h4><b>Consulting</b></h4>
-                        <p>Wir lösen Probleme für Sie</p>
-                        <br>
-                        </div>
-                        <div id="picture" class="col-sm-6 col-md-3">
-                            <div class="document"></div>
-
-                            <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/Output Management')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/print.png" alt="document" width="150" height="150" class="alignnone size-thumbnail wp-image-182" /></a>
-                            <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/outputmanagement')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/print1.png" alt="document" width="150" height="150" class="alignnone size-thumbnail wp-image-182" /></a>
-                            <br>
-                            <h4><b>Output Management</b></h3>
-                                <p>Wir optimieren und verbessern für Sie</p>
+                        <br><br>
+                        <div class="col-sm-6 col-md-3">
+                            <div class="thumbnail">
+                                <div class="user"> </div>
+                                <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/consulting')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/user.png" alt="user" width="150" height="150" class="alignnone size-thumbnail wp-image-185" /></a>
+                                <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/consulting')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/user3.png" alt="user2" width="150" height="150" class="alignnone size-thumbnail wp-image-185" /></a>
+                                <h4><b>Consulting</b></h4>
+                                <p>Wir lösen Probleme für Sie</p>
                                 <br>
-                                </div>
-                                <div id="picture" class="col-sm-6 col-md-3">
-                                    <div class="keyboard"></div>
-                                    <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/Software Engineering')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/keyboard.png" alt="keyboard" width="150" height="150" class="alignnone size-thumbnail wp-image-183" /></a>
-                                    <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/softwareengineering')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/keyboard1.png" alt="keyboard" width="150" height="150" class="alignnone size-thumbnail wp-image-183" /></a>
-                                    <br>
-                                    <h4><b>Software Engineering</b></h3>
-                                        <p>Wir entwickeln für Sie</p>
-                                        <br>
-                                        </div>
-                                        <div id="picture" class="col-sm-6 col-md-3">
-                                            <div class="network"></div>
-                                            <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/infrastruktur')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/network.png" alt="network" width="150" height="150" class="alignnone size-thumbnail wp-image-184" /></a>
-                                            <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/infrastruktur')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/network1.png" alt="network" width="150" height="150" class="alignnone size-thumbnail wp-image-184" /></a>
-                                            <br>
-                                            <h4><b>IT-Infrastruktur</b> </h3>
-                                                <p>Wir verbinden Sie</p>
-                                        </div>
-                                </div>
+                            </div>
                         </div>
-        </div>
+
+                        <div id="picture" class="col-sm-6 col-md-3">
+                            <div class="thumbnail">
+                                <div class="document"></div>
+
+                                <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/Output Management')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/print.png" alt="document" width="150" height="150" class="alignnone size-thumbnail wp-image-182" /></a>
+                                <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/outputmanagement')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/print1.png" alt="document" width="150" height="150" class="alignnone size-thumbnail wp-image-182" /></a>
+                                <br>
+                                <h4><b>Output Management</b></h3>
+                                    <p>Wir optimieren und verbessern für Sie</p>
+                                    <br>
+                             </div>
+                        </div>
+                      <div id="picture" class="col-sm-6 col-md-3">
+                          <div class="thumbnail">
+                                        <div class="keyboard"></div>
+                                        <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/Software Engineering')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/keyboard.png" alt="keyboard" width="150" height="150" class="alignnone size-thumbnail wp-image-183" /></a>
+                                        <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/softwareengineering')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/keyboard1.png" alt="keyboard" width="150" height="150" class="alignnone size-thumbnail wp-image-183" /></a>
+                                        <br>
+                                        <h4><b>Software Engineering</b></h3>
+                                            <p>Wir entwickeln für Sie</p>
+                                            <br>
+                                            </div>
+                     </div>
+                     <div id="picture" class="col-sm-6 col-md-3">
+                         <div class="thumbnail">
+                                                <div class="network"></div>
+                                                <a class="nonhoverpic" href="<?php echo get_permalink(get_page_by_path('leistungen/infrastruktur')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/network.png" alt="network" width="150" height="150" class="alignnone size-thumbnail wp-image-184" /></a>
+                                                <a class="hoverpic" style="display:none" href="<?php echo get_permalink(get_page_by_path('leistungen/infrastruktur')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/network1.png" alt="network" width="150" height="150" class="alignnone size-thumbnail wp-image-184" /></a>
+                                                <br>
+                                                <h4><b>IT-Infrastruktur</b> </h3>
+                                                    <p>Wir verbinden Sie</p>
+                                            </div>
+                                    </div>
+                            </div>
+                        </div>
 
 <!--
         <div id="main">
