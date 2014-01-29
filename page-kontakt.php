c796c84bbc26a2cf5371bb5353de7056d62cd69a
itp1tkl <thomas.kleinert@it-power.org>
2014-01-29 12:32:19 +0100

    update

--- page-kontakt.php
+++ page-kontakt.php
@@ -79,7 +79,7 @@
                                 <br>
                             </div></div></div>
                                         
-                    <div class="col-sm-4 col-md-4">	 	 
+                    <div class="col-sm-3 col-md-3">	 	 
                         <div class="thumbnail">	 	 
                             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/lisa.jpg" alt="BILD" width="150" height="100" class="alignnone size-thumbnail wp-image-182">		 	 
                             <div class="caption">	 	 
