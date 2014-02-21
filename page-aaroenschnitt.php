f1ca4b95cce2ba0ce92507a6a2331cf9ca205178
itp1tkl <thomas.kleinert@it-power.org>
2014-02-21 12:03:00 +0100

    update

---
+++ page-aaroenschnitt.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <div id="intro-small" class="hamburg">
+            <div class="row">
+                <div class="background5">
+                    <h1>Dienstleistungen</h1>
+                </div>
+            </div>
+        </div>
+
+        <div id="main" style="text-align: justify;"> 
+            <?php the_content(); ?>
+        </div>
+
+    <?php endwhile;
+else: ?>
+    <p><?php _e('Tut uns Leid. Die Seite existiert nicht.'); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
