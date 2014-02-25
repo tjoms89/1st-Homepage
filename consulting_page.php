5ca7e9ec4eb87ee02c86d8f5f89e5cd754561451
itp1tkl <thomas.kleinert@it-power.org>
2014-02-25 09:22:26 +0100

    update

--- consulting_page.php
+++ consulting_page.php
@@ -17,10 +17,10 @@ Template Name: Consulting Template
     
                     <ul class="content-navigation" >
                         <li id="1"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/projektmanagement')); ?>">Projektmanagement</a></li>
-                        <li id="2"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/geschaftsprozessmanagement')); ?>">Geschäftsprozessmanagement</a></li>
+                        <li id="2"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/geschaftsprozessmanagement')); ?>">Geschäftsprozessmanagement</a></li><br>
                         <li id="3"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/requirements-engineering')); ?>">Requirements Engineering</a></li>
                         <li id="4"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/change-management')); ?>">Change Management</a></li>
-                        <li id="5"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/qualitätsmanagement')); ?>">Qualitätsmanagement</a></li>
+                        <li id="5"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/qualitätsmanagement')); ?>">Qualitätsmanagement</a></li><br>
                         <li id="6"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/sap-berechtigungswesen')); ?>">SAP Berechtigungswesen</a></li>
                         <li id="7"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/erp')); ?>">ERP</a></li>
                         <li id="8"><a href="<?php echo get_permalink(get_page_by_path('leistungen/consulting/energiedatenmanagement')); ?>">Energiedatenmanagement</a></li>
@@ -34,6 +34,7 @@ Template Name: Consulting Template
 
                 <br>
             <?php the_content(); ?>
+                <br>
             </div>
         </div>
 
