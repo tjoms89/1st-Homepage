0114a72b3f0029c706655a5c27c07fb6b37b2a05
itp1tkl <thomas.kleinert@it-power.org>
2014-02-27 11:37:16 +0100

    update

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
     <div class="input-group">
         <input type="text" style="background-color: #2d438e; color: #FFFFFF; border: 1px solid #FFFFFF;" class="form-control" id="s" name="s" placeholder="Suche auf it-power.org"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
         <span class="input-group-btn">
-            <button type="submit" class="btn btn-primary" style="background-color: #2d438e !important; background-image: none; color: #FFFFFF !important; border: 1px solid #FFFFFF !important;" ><i class="icon-search">Suche</i></button>
+            <button type="submit" class="btn" style="background-color: #2d438e !important; background-image: none; color: #FFFFFF !important; border: 1px solid #FFFFFF !important;" ><i class="icon-search">Suche</i></button>
         </span>
     </div> <!-- .input-group -->
 </form>
