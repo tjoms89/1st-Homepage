a3b54c08f8e530f9a7fe2842bcbc500093ffc1ea
itp1tkl <thomas.kleinert@it-power.org>
2014-03-04 12:54:26 +0100

    updated print function

--- consulting_page.php
+++ consulting_page.php
@@ -44,7 +44,7 @@ Template Name: Consulting Template
             <?php the_content(); ?>
                 <br>
                  <br>
-                <input id="btntest" type="button" value="druck mich" style="height: 25px; border:none;  background-color: #2d438e; color: #FFFFFF;"/>
+                <input id="btntest" type="button" value="drucken" style="height: 25px; border:none;  background-color: #2d438e; color: #FFFFFF;"/>
                 <br>
                  <br>
                   <br>
@@ -56,24 +56,32 @@ Template Name: Consulting Template
             jQuery(function(){
                     
                      jQuery('#btntest').on('click', function() {
-                        var php1 = JSON.stringify(
+                        var php = JSON.stringify(
                                                     <?php
-                                                        $jsonpost = array();
-                                                        $jsonpost[content] = apply_filters('the_content', get_the_content());
-                                                        echo json_encode($jsonpost[content]); 
+                                                        $string = array();
+                                                        $string[content] = apply_filters('the_content', get_the_content());
+
+                                                        echo json_encode($string[content]);
                                                     ?>
                          );
-                         
-                        var newphp = php1.replace('"','');
-                        newphp = newphp.substring(0, newphp.length - 3);
-                        
+                             
+                         var escaped = php.replace(/\\n/g, "")
+                                      .replace(/\\'/g, "")
+                                      .replace(/\\"/g, '')
+                                      .replace(/\\&/g, "")
+                                      .replace(/\\r/g, "")
+                                      .replace(/\\t/g, "")
+                                      .replace(/\\b/g, "")
+                                      .replace(/\\f/g, "")
+                                      .replace('"', '');
+
                         myWindow=window.open('','','width=auto,height=auto');
                         myWindow.document.write('<div style="width:60%; margin-left:auto; margin-right:auto;">');
                         myWindow.document.write('<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/itplogo2.png" alt="logo" width="235" height="30" class="alignnew"/>');
-                        myWindow.document.write(newphp);
+                        myWindow.document.write(escaped);
                         myWindow.document.write('</div>');
                         myWindow.focus();
-                        myWindow.print(); 
+                        //myWindow.print(); 
                     });
                     });
             </script>
