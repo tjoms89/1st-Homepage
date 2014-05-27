786adb1adb5a39acd039dd40a9735dba626ab46d
itp1tkl <thomas.kleinert@it-power.org>
2014-05-27 12:22:29 +0200

    update

--- page-kontakt.php
+++ page-kontakt.php
@@ -86,7 +86,7 @@
                                        
                     <div class="col-sm-3 col-md-3">	 	
                         <div class="thumbnail">	 	 
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/frederic.jpg" alt="BILD" width="150" height="100" class="alignnone size-thumbnail ">		 	 
+                            <img style="border: 1px solid;" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/frederic.jpg" alt="BILD" width="145" height="96" class="alignnone size-thumbnail ">		 	 
                             <div class="caption">	 	 
                                 <h3>Frederic M. Thordsen</h3>	 	 
                                 <p style="font-weight: bold;">Vertrieb</p>
