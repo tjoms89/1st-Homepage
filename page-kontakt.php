ce222c3bf394465658d6df58c20a54a0e3e3cc0e
itp1tkl <thomas.kleinert@it-power.org>
2014-05-27 13:29:49 +0200

    update

--- page-kontakt.php
+++ page-kontakt.php
@@ -56,9 +56,7 @@
                     
                     <div class="col-sm-3 col-md-3">	 	
                         <div class="thumbnail">
-                            <div style="border: 1px solid">
-                            <img style="min-height: 150px; max-height: 150px; margin-top: 30px;" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/karsten.png" alt="BILD" width="145" height="96" class="alignnone size-thumbnail ">		 	 
-                            </div>
+                            <img style="border: 1px solid; min-height: 150px; max-height: 150px;" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/karsten.png" alt="BILD" width="145" height="96" class="alignnone size-thumbnail ">		 	 
                             <div class="caption">	<br>
                                 <h3>Karsten Milewsky</h3>	 	 
                                 <p style="font-weight: bold;">Leiter Output Management</p>
@@ -93,10 +91,11 @@
                                 <h3>Frederic M. Thordsen</h3>	 	 
                                 <p style="font-weight: bold;">Vertrieb</p>
                                 <br>
-                                <p>Tel.: +49  461/487-3434</p>
+                                <p>Tel.: +49  461/487-3430</p>
                                 <p>Fax: +49  461/487-2424</p>
+                                <p>Mobil: +49 170/7828153</p>
                                 <p><a href="mailto:frederic.thordsen@it-power.org">frederic.thordsen@it-power.org</a></p>
-                                <br><br><br><br>
+                                <br><br><br>
                             </div></div></div>	
 
                 </div>
